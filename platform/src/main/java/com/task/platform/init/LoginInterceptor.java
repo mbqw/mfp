f1d2930239a9ac2d71fb07959d4f9dbdd3216dd5
wubo <wb322@foxmail.com>
2019-03-20 18:01:56 +0800

    优化

--- platform/src/main/java/com/task/platform/init/LoginInterceptor.java
+++ platform/src/main/java/com/task/platform/init/LoginInterceptor.java
@@ -1,6 +1,5 @@
 package com.task.platform.init;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
--- platform/src/main/java/com/task/platform/init/WebSecurityConfig.java
+++ platform/src/main/java/com/task/platform/init/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.task.platform.init;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebSecurityConfig implements WebMvcConfigurer {
@@ -16,6 +17,12 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(getLoginInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/common/*","/images/**","/static/**","/templete/index.html");
+                .excludePathPatterns("/common/*","/css/**","/fonts/**","/images/**","/js/**","/layui/**");
     }
+
+    /*@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }*/
 }
